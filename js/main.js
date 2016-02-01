4e96bc7203d1a70bda7a0600e064c8627e77dd69
martin <tlkuo.frb@gmail.com>
2016-02-01 15:44:58 +0800

    Add Gravatar image to header - close #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 (function () {
 	'use strict';
 
+  $("img[avatar]").attr("src", function () {
+    var email = $(this).attr("avatar").trim().toLowerCase(),
+        hash = md5(email),
+        width = $(this).width(),
+        height = $(this).height(),
+        size = width <= height ? width : height;
+
+    return "//www.gravatar.com/avatar/" + hash + "?size=" + size;
+  });
+
 	$(document).foundation();
 })();
